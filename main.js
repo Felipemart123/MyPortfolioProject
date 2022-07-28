9159bd237b7a417fbdca9537b507c6765548d0a4
Felipemart123 <felipemart1876@gmail.com>
2022-07-28 14:14:23 -0400

    resume commit

--- main.js
+++ main.js
@@ -24,32 +24,32 @@ $('#switch1').on('click', function(){
 
 
 // If user visit after 7 pm in night the body will change the style to night class
-$(document).ready(function(){
-  var date = new Date();
-  var current_time = date.getHours();
-  if (current_time > 19 || current_time < 6)
-    // If time is after 7PM or before 6AM, apply night theme to ‘body’
-    document.body.className = "night";
-  else
-    // Else use ‘day’ theme
-    document.body.className = "";
-});
+// $(document).ready(function(){
+//   var date = new Date();
+//   var current_time = date.getHours();
+//   if (current_time > 19 || current_time < 6)
+//     // If time is after 7PM or before 6AM, apply night theme to ‘body’
+//     document.body.className = "night";
+//   else
+//     // Else use ‘day’ theme
+//     document.body.className = "";
+// });
 
-$(window).load(function() {
-      $("#top-button").hide();
-});
+// $(window).load(function() {
+//       $("#top-button").hide();
+// });
 
 
 // If you hover over the languages I used in project Box it will do fade in animation
-$('.project_used span').on({
-    mouseover: function() {
-        event.preventDefault();
-        $(this).animate({opacity: 0.25});
-    },
-    mouseout: function() {
-        event.preventDefault();
-        $(this).animate({opacity: 1});
-    }
-});
+// $('.project_used span').on({
+//     mouseover: function() {
+//         event.preventDefault();
+//         $(this).animate({opacity: 0.25});
+//     },
+//     mouseout: function() {
+//         event.preventDefault();
+//         $(this).animate({opacity: 1});
+//     }
+// });
 
 
